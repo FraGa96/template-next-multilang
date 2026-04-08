7358490c423e53b3178faa2ebe103173def2212d
FraGa96 <andy_gfg@hotmail.com>
2026-04-08 11:30:07 -0600

    add svg support

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,23 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: { test?: RegExp }) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    if (fileLoaderRule) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/, 
+        use: ['@svgr/webpack'],
+      });
+
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
